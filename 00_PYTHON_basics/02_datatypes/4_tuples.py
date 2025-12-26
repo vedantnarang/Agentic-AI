f82ee0022a49177033dc53e5f33196e75453cc6a
Vedant Narang <vedant.narang36@gmail.com>
2025-12-26 15:02:14 +0530

    first

--- 00_PYTHON_basics/02_datatypes/4_tuples.py
+++ 00_PYTHON_basics/02_datatypes/4_tuples.py
@@ -1,6 +1,19 @@
 #tuples means()
 
 
-spices=("cardamom","chilli","cinnamon","haldi");
+print('*-'*5,'TUPLES')
+tuple=(5,6,7,8,9)
+print(tuple[0])
+print(tuple[2])
+print(tuple[4])
+print(2 in tuple)
+print(5 in tuple)
+print(len(tuple))
 
-print(spices[2])
+
+#concatenate
+tpl1=(1,2,3,4)
+tpl2=(4,5,6,7)
+tpl=tpl1+tpl2
+print(tpl)
+print('-'*25)
